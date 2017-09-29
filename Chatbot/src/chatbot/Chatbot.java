c233aa1fffd99c44c9b92bff91a31fa33838c757
BT_1N3_16 <BT_1N3_16@BT_1N3_16.bktech.local>
2017-09-29 14:46:39 -0400

    Chatbot

---
+++ Chatbot/src/chatbot/Chatbot.java
@@ -0,0 +1,14 @@
+package chatbot;
+
+public class Chatbot {
+	
+	private String username;
+	private boolean chatting;
+	private Topic jasony;
+	
+	public Chatbot() {
+		jasony = new ChatbotJasonY();
+		username = "Unknown User";
+		chatting = ;
+	}
+}
